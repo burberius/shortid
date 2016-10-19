f54d8dc8a2c53741ebe7b7a2dd1f2dc3fbd8f85f
Jens Oberender <j.obi@troja.net>
2016-10-19 22:16:39 +0200

    Initial commit.

---
+++ servicea/src/main/java/net/troja/application/servicea/ShortidApplication.java
@@ -0,0 +1,12 @@
+package net.troja.application.servicea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortidApplication.class, args);
+	}
+}
